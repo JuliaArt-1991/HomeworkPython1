0facafb584571d919339bd0c802009bd177db4fd
Julia <artamon-julia@ya.ru>
2023-04-29 18:50:56 +0700

    corrected

---
+++ Task2.py
@@ -0,0 +1,4 @@
+time_sec = int(input('Введите число: '))
+hours = time_sec//3600
+minutes = time_sec//60
+print(f'Время: {hours}:{minutes}:{time_sec}')
---
+++ Task3.py
@@ -0,0 +1,6 @@
+n = input('Введите число: ')
+nn = int(n+n)
+nnn = int(n+n+n)
+n = int(n)
+result = n+nn+nnn
+print(result)
